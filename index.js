3c011d6c3fef4aca4d93f95c08cd226818c9fa0d
yuulye <yuulye@gmail.com>
2020-01-27 22:34:15 +0700

    serve webpage at /

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ try {
 const { prefix, token } = config;
 client.login(token);
 
+const http = require('http');
+const server = http.createServer(function (req, res) {
+  const url = req.url;
+  if (url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('0.0.1\n');
+  } else {
+    res.statusCode = 404;
+    res.end('404, sorry!\n');
+  }
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
+});
+
 client.once('ready', () => {
   console.log(`logged in as ${client.user.tag}!`);
 });

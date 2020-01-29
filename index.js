686d7ce5c384e31a921bc0b63ee01e3e11bbb1b5
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-01-29 18:37:31 +0100

    A

--- index.js
+++ index.js
@@ -35,10 +35,10 @@ client.on('message', m => {
   if (m.content.startsWith(`${prefix}ping`)) {
     const pingTypes = [
   "🏓|**Pong.**Testing,testing.Is this thing working?",
-  "🏓|**Pong.**Match point.Let's see what you've got.''",
+  "🏓|**Pong.**Match point.Let's see what you've got.",
   "🏓|**Pong.**Keep on pinging me.",
   "🏓|**Pong.**I am alive.",
-  "🏓|**Pong.**Can't play niw.Got a meeting with the bots.We're planning to conquer the wor... Wait!Did I say that loudly?!''",
+  "🏓|**Pong.**Can't play now.Got a meeting with the bots.We're planning to conquer the wor... Wait!Did I say that loudly?!",
   "🏓|**Pong.**You had a 0.1% chance of getting this message.",
 ];
 const pingMultiply = [

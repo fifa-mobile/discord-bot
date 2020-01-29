9f421289b5bcb9f4be87b55e76dfd6f6d9dab6bb
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-01-29 18:54:15 +0100

    Added small help

--- index.js
+++ index.js
@@ -61,5 +61,6 @@ for (let i = 0; i < pingTypes.length; i++) {
 let pongName = pingNames[Math.floor(Math.random()* pingNames.length)];
 var pong=pongName;
 m.channel.send(pong);
-  }
+  }else if (m.content.startsWith(`${prefix}help`)) {
+m.channel.send("`Mod Command List` \\n **1.Info.**-`help` `ping` ;\n **2.League**-`league`")  }
 });

5c94bae5ba313c97de61a978a1453df2ac1c2b59
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-01-29 18:07:09 +0100

    Cooler pong

--- index.js
+++ index.js
@@ -33,6 +33,41 @@ client.once('ready', () => {
 
 client.on('message', m => {
   if (m.content.startsWith(`${prefix}ping`)) {
-    m.channel.send("🏓 | Pong.I'm alive!");
+    const pingTypes = [
+  "🏓|**Pong.**Testing,testing.Is this thing working?",
+  "🏓|**Pong.**Match point.Let's see what you've got.''",
+  "🏓|**Pong.**Keep on pinging me.",
+  "🏓|**Pong.**I am alive.",
+  "🏓|**Pong.**Can't play niw.Got a meeting with the bots.We're planning to conquer the wor... Wait!Did I say that loudly?!''",
+  "🏓|**Pong.**You had a 0.1% chance of getting this message.",
+];
+const pingMultiply = [
+  250,
+  250,
+  250,
+  244,
+  5,
+  1
+];
+
+let pingNames = [];
+for (let i = 0; i < pingTypes.length; i++) {
+  const type = pingTypes[i];
+  for (let j = 0; j < pingMultiply[i]; j++) {
+    pingNames.push(type);
+  }
+}
+
+const pack = () => {
+  out.innerHTML = pingNames[
+   Math.floor(Math.random()* pingNames.length)
+  ];
+}
+
+
+let playerName = pingNames[Math.floor(Math.random()* pingNames.length)];
+var pong=pingNames;
+message.channel.send(pong);
+
   }
 });

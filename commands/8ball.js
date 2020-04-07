e162dc818451404cc43df0714b5a4b543d9330fa
yuulye <yuulye@gmail.com>
2020-04-08 01:31:42 +0700

    refactor 8ball; new command: read

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,22 +1,22 @@
 module.exports = (_y, args) => {
-   if(args[0] && args[0] !== null) {
-     const answers = [
-       "Yep!",
-       "You may rely on it.",
-       "Without a doubt!",
-       "As I see it,YES.",
-       "Concentrate and ask again.",
-       "I didn't get it.Could you ask again please?",
-       "My sources say NO.",
-       "Don't count on it.",
-       "Outlook not so good.",
-       "Very doubtful.",
+  if(args[0] && args[0] !== null) {
+    const answers = [
+      "Yep!",
+      "You may rely on it.",
+      "Without a doubt!",
+      "As I see it,YES.",
+      "Concentrate and ask again.",
+      "I didn't get it.Could you ask again please?",
+      "My sources say NO.",
+      "Don't count on it.",
+      "Outlook not so good.",
+      "Very doubtful.",
     ];
-     let answer = answers[
-       Math.floor(Math.random()* answers.length)
-     ];
-     _y.reply('🎱 | ' + answer);
-   } else {
-     _y.reply('What do you want to ask?');
-   }
+    let answer = answers[
+      Math.floor(Math.random()* answers.length)
+    ];
+    _y.reply('🎱 | ' + answer);
+  } else {
+    _y.reply('What do you want to ask?');
+  }
 };
---
+++ commands/read.js
@@ -0,0 +1,34 @@
+const y = require('../core/base');
+
+module.exports = (_y, args) => {
+  let msg = _y.message;
+  let channel = y.client.channels.find(
+    ch => ch.name === 'x' || ch.name === 'one_word_story'
+  );
+  if (
+    msg.channel.name === 'x'
+    || msg.channel.name === 'one_word_story'
+    || true
+  ) {
+    channel.fetchMessages().then(
+      messages => {
+        console.log('messages count: '+messages.size);
+        let story = [];
+        let msgs = messages.array();
+        for (let i = 0; i < msgs.length; i++) {
+          let m = msgs[i];
+          if (
+            m.toString() === '$read'
+            || m.toString().startsWith('.....')
+          ) continue;
+          story.push(m);
+        }
+        story.reverse();
+        msg.channel.send('.....'+story.join(' '));
+      }
+    );
+  } else {
+    msg.channel.send("can't arrange the story here...");
+  }
+  console.log('story end...', msg.channel.name);
+};

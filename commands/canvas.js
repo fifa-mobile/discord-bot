23becc3305f845dbc141899cf72a2732cd813541
yuulye <yuulye@gmail.com>
2020-04-26 21:50:10 +0700

    init chess

--- commands/canvas.js
+++ commands/canvas.js
@@ -1,39 +1,41 @@
-module.exports = (_y, args) => {
+module.exports = async (_y, args) => {
   const y = require('../core/base');
   const D = y.Discord;
   const fs = require('fs')
   const { createCanvas, loadImage } = require('canvas')
 
-  const width = 1200
-  const height = 630
+  const w = 578;
+  const h = 578;
 
-  const canvas = createCanvas(width, height)
-  const context = canvas.getContext('2d')
+  const canvas = createCanvas(w, h);
+  const context = canvas.getContext('2d');
 
-  context.fillStyle = '#000'
-  context.fillRect(0, 0, width, height)
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, w, h);
 
-  context.font = 'bold 70pt Menlo'
-  context.textAlign = 'center'
-  context.textBaseline = 'top'
-  context.fillStyle = '#3574d4'
+  context.font = 'bold 70pt Menlo';
+  context.textAlign = 'center';
+  context.textBaseline = 'top';
+  context.fillStyle = '#3574d4';
 
-  const text = 'Hello, World!'
+  const text = 'Hello, World!';
 
-  const textWidth = context.measureText(text).width
-  context.fillRect(600 - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120)
+  const textWidth = context.measureText(text).width;
+  context.fillRect(
+    600 - textWidth / 2 - 10, 170 - 5
+    , textWidth + 20, 120
+  );
   context.fillStyle = '#fff'
-  context.fillText(text, 600, 170)
+  context.fillText(text, 600, 170);
 
   context.fillStyle = '#fff'
   context.font = 'bold 30pt Menlo'
   context.fillText('flaviocopes.com', 600, 530)
 
-  loadImage('./logo.png').then(image => {
-    context.drawImage(image, 340, 515, 70, 70)
-    const buffer = canvas.toBuffer('image/png')
-    const attachment = new D.Attachment(buffer, 'x.png');
-    _y.reply(`Welcome!`, attachment);
-    fs.writeFileSync('./test.png', buffer)
-  })
+  const logo = await loadImage('./images/logo-wa.png');
+  context.drawImage(logo, 340, 515, 70, 70)
+
+  const buffer = canvas.toBuffer('image/png')
+  const attachment = new D.Attachment(buffer, 'x.png');
+  _y.reply({files: [attachment]});
 };
--- commands/chess.js
+++ commands/chess.js
@@ -1,3 +1,17 @@
+function toArray(string) {
+  const lines = string.split("\n");
+  let board = [];
+  for (let i = 0; i < lines.length; i++) {
+    let squares = lines[i].split('');
+    for (let j = squares.length - 1; j >= 0; j--) {
+      if (j % 2 === 0) continue;
+      squares.splice(j, 1);
+    }
+    board.push(squares);
+  }
+  return board;
+}
+
 module.exports = async (_y, args) => {
   const
     y = require('../core/base'),
@@ -9,38 +23,101 @@ module.exports = async (_y, args) => {
   ;
 
   let status = gameClient.getStatus();
-  let validMoves = status.notatedMoves;
 
-  const stringBoard = chessDisplay.toAscii();
+  const boardArray = toArray(chessDisplay.toAscii());
 
-  _y.reply(
-    '```' +
-    stringBoard
-    + '```'
+  const Canvas = require('canvas');
+  const canvas = Canvas.createCanvas(578, 578);
+  const ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const [mapNumbers, mapChars] = [
+    '12345678',
+    'abcdefgh',
+  ];
+
+  ctx.font = 'bold 30pt Serif';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = '#fff';
+
+  let [posX, posY] = [32, 546];
+  for (let i = 0; i < 8; i++) {
+    ctx.fillText(mapNumbers[i], posX, posY);
+    posY -= 64;
+  }
+
+  [posX, posY] = [96, 32];
+  for (let i = 0; i < 8; i++) {
+    ctx.fillText(mapChars[i], posX, posY);
+    posX += 64;
+  }
+
+  const boardImage = await Canvas.loadImage(
+    './images/chess/board.png'
+  );
+  ctx.drawImage(
+    boardImage, 64, 64, 512, 512 
   );
 
+  posY = 64;
+  for (let i = 0; i < boardArray.length; i++) {
+    posX = 64;
+    const boardLine = boardArray[i];
+    for (let j = 0; j < boardLine.length; j++) {
+      const squareValue = boardLine[j];
+      const imgPath = tPiece(squareValue);
+      if (imgPath) {
+        const piece = await Canvas.loadImage(imgPath);
+        ctx.drawImage(piece, posX, posY, 64, 64);
+      }
+      posX += 64;
+    }
+    posY += 64;
+  }
+
+  const attachment = new D.Attachment(
+    canvas.toBuffer(), 'img.png'
+  );
+
+  let validMoves = [];
   Object.keys(
     gameClient.notatedMoves
   ).forEach(notation => {
-    console.log(notation);
+    validMoves.push('`' + notation + '`');
   });
 
-  const Canvas = require('canvas');
-  const canvas = Canvas.createCanvas(700, 250);
-  const ctx = canvas.getContext('2d');
-  const background = await Canvas.loadImage(
-    './wallpaper.jpg'
-  );
-  ctx.drawImage(
-    background, 0, 0, canvas.width, canvas.height
-  );
-  const attachment = new D.Attachment(
-    canvas.toBuffer(), 'welcome-image.png'
-  );
+  let turn = 'white';
 
-  console.log(attachment);
-
-  _y.reply({
-    files: [attachment]
-  });
+  _y.message.channel.send(
+    `Valid **_${turn}_** moves:`
+    + ` ${validMoves.join(' , ')}`
+    , {
+      files: [attachment]
+    }
+  );
 };
+
+function tPiece(char) {
+  let path = './images/chess/pieces/';
+  const map = {
+    '♜': 'black-rook',
+    '♞': 'black-knight',
+    '♝': 'black-bishop',
+    '♛': 'black-queen',
+    '♚': 'black-king',
+    '♟': 'black-pawn',
+    /* ------------- */
+    '♙': 'white-pawn',
+    '♖': 'white-rook',
+    '♘': 'white-knight',
+    '♗': 'white-bishop',
+    '♕': 'white-queen',
+    '♔': 'white-king',
+  };
+  const value = map[char];
+  if (!value) return false;
+  return `./images/chess/pieces/${value}.png`;
+}

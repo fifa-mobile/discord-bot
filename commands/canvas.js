46d1d2c8b080c4164a1ea4675a669c7cc7fbd504
yuulye <yuulye@gmail.com>
2020-04-27 22:58:25 +0700

    new command

--- commands/canvas.js
+++ commands/canvas.js
@@ -1,41 +1,40 @@
 module.exports = async (_y, args) => {
   const y = require('../core/base');
   const D = y.Discord;
-  const fs = require('fs')
-  const { createCanvas, loadImage } = require('canvas')
+  const { createCanvas, loadImage } = require('canvas');
 
-  const w = 578;
-  const h = 578;
+  const w = 1200;
+  const h = 630;
 
   const canvas = createCanvas(w, h);
-  const context = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d');
 
-  context.fillStyle = '#000';
-  context.fillRect(0, 0, w, h);
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, w, h);
 
-  context.font = 'bold 70pt Menlo';
-  context.textAlign = 'center';
-  context.textBaseline = 'top';
-  context.fillStyle = '#3574d4';
+  ctx.font = 'bold 70pt Menlo';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'top';
+  ctx.fillStyle = '#3574d4';
 
   const text = 'Hello, World!';
 
-  const textWidth = context.measureText(text).width;
-  context.fillRect(
+  const textWidth = ctx.measureText(text).width;
+  ctx.fillRect(
     600 - textWidth / 2 - 10, 170 - 5
     , textWidth + 20, 120
   );
-  context.fillStyle = '#fff'
-  context.fillText(text, 600, 170);
+  ctx.fillStyle = '#fff';
+  ctx.fillText(text, 600, 170);
 
-  context.fillStyle = '#fff'
-  context.font = 'bold 30pt Menlo'
-  context.fillText('flaviocopes.com', 600, 530)
+  ctx.fillStyle = '#fff';
+  ctx.font = 'bold 30pt Menlo';
+  ctx.fillText('flaviocopes.com', 600, 530);
 
   const logo = await loadImage('./images/logo-wa.png');
-  context.drawImage(logo, 340, 515, 70, 70)
+  ctx.drawImage(logo, 340, 515, 70, 70);
 
-  const buffer = canvas.toBuffer('image/png')
+  const buffer = canvas.toBuffer('image/png');
   const attachment = new D.Attachment(buffer, 'x.png');
   _y.reply({files: [attachment]});
 };
---
+++ commands/list.js
@@ -0,0 +1,112 @@
+module.exports = async (_y, args) => {
+  const {User} = require('../models/index');
+  const uid = _y.message.author.id;
+  const user = await User.findOne({where: {uid: uid}});
+  const y = require('../core/base');
+  const D = y.Discord;
+  const { createCanvas, loadImage } = require('canvas');
+
+  const w = 1280;
+  const h = 500;
+
+  const canvas = createCanvas(w, h);
+  const ctx = canvas.getContext('2d');
+
+  const types = {
+    1: {
+      price: 1,
+      image: "bronze",
+    },
+    2: {
+      price: 5,
+      image: "silver",
+    },
+    3: {
+      price: 10,
+      image: "gold",
+    },
+    4: {
+      price: 15,
+      image: "elite",
+    },
+    5: {
+      price: 30,
+      image: "elite85",
+    },
+    6: {
+      price: 50,
+      image: "master",
+    },
+    7: {
+      price: 100,
+      image: "master95",
+    },
+    8: {
+      price: 5000,
+      image: "legend",
+    },
+    9: {
+      price: 200,
+      image: "icon",
+    },
+    10: {
+      price: 10000,
+      image: "prime",
+    },
+  };
+
+  ctx.font = 'bold 50pt monospace';
+  ctx.textAlign = 'right';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = '#eee';
+  ctx.strokeStyle = '#111';
+  ctx.lineWidth = 16;
+
+  const packs = await user.getPacks();
+  let [posX, posY] = [128, 8];
+  let resetX = true;
+  let total = 0;
+  for (let i = 10; i > 0; i--) {
+    const pack = packs.filter(
+      pack => pack.packid === i
+    );
+    let amount = 0;
+    if (pack.length) {
+      amount = pack[0].amount;
+    }
+    if (i < 6) {
+      posY = 180;
+      if (resetX) {
+        posX = 128;
+        resetX = false;
+      }
+    }
+    const type = types[i];
+    const path = `./images/pack/${type.image}.png`;
+    const img = await loadImage(path);
+    ctx.strokeText(amount, posX - 5, posY + 60);
+    ctx.fillText(amount, posX - 5, posY + 60);
+    ctx.drawImage(img, posX, posY, 128, 128);
+    posX += 256;
+    total += amount * type.price;
+  }
+
+  ctx.fillStyle = "#eee";
+  ctx.fillRect(0, 340, canvas.width, 8);
+
+  ctx.font = 'bold 50pt serif';
+  ctx.textAlign = 'left';
+  ctx.textBaseline = 'middle';
+  ctx.fillStyle = "#eee";
+
+  ctx.fillText("total value: $", 90, 410);
+
+  ctx.font = 'bold 50pt monospace';
+  ctx.fillStyle = "#fffe4c";
+
+  ctx.fillText(total, 590, 414);
+
+  const buffer = canvas.toBuffer('image/png');
+  const attachment = new D.Attachment(buffer, 'x.png');
+  _y.reply({files: [attachment]});
+};

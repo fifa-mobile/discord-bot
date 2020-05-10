9b210538fb6282e0351c0ec390814ad9acfd5e42
yuulye <yuulye@gmail.com>
2020-05-11 00:20:40 +0700

    update

--- core/web.js
+++ core/web.js
@@ -10,50 +10,15 @@ class Web {
     const port = process.env.PORT || 3000;
 
     const express = require('express');
-    const _handlebars = require('handlebars');
-    const exphbs  = require('express-handlebars');
-    const {allowInsecurePrototypeAccess} = require(
-      '@handlebars/allow-prototype-access'
-    );
     const app = express();
+    const path = require('path');
 
-    const urlShortener = require('node-url-shortener');
-    const bodyParser = require('body-parser');
-    const db = require('../models/index.js');
-
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(express.urlencoded());
-    app.engine('handlebars', exphbs({
-      handlebars: allowInsecurePrototypeAccess(
-        _handlebars
-      )
-    }));
-    app.set('view engine', 'handlebars');
-
-    app.get('/', function(req, res) {
-      db.Url.findAll({
-        order: [['createdAt', 'DESC']], limit: 5
-      }).then(urlObjs => {
-        res.render('index', {
-          urlObjs: urlObjs
-        });
-      });
-    });
-
-    app.post('/url', function(req, res) {
-      const url = req.body.url
-
-      urlShortener.short(url, function(err, shortUrl) {
-        db.Url.findOrCreate({
-          where: {url: url, shortUrl: shortUrl}
-        }).then(([urlObj, created]) => {
-          res.redirect('/');
-        });
-      });
-    });
+    app.use(express.static(path.join(
+      __dirname, '../public'
+    )));
 
     app.listen(port, () => console.log(
-      `url-shortener listening on port ${port}!`
+      `listening on port ${port}!`
     ));
   }
 }

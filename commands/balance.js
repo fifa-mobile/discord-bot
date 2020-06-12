d7a66b0b8817dd27d4298bf753c73a69600f5321
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-06-12 19:05:48 +0200

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -20,10 +20,11 @@ module.exports = async (_y, args) => {
   var uid;
   if(!args[1]){
   uid = _y.message.author.id;
+  const user = await User.findOne({where: {uid: uid}});
   }else{ 
   uid = getUser(args[1]);
-  }
   const user = await User.findOne({where: {uid: uid}});
+  }
   if (!user) {
     return _y.reply(`User not found!`);
   }

bbb60459b9f6d0bc3569fe5bfe8c51da7c41b131
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-06-12 11:30:35 +0000

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,6 +1,22 @@
 const db = require('../models/index.js');
 const User = db.User;
 
+function getUser(client, str) {
+	if (!str) return false;
+
+  if (
+    str.startsWith('<@') && str.endsWith('>')
+  ) {
+		str = str.slice(2, -1);
+
+		if (str.startsWith('!')) {
+			str = str.slice(1);
+		}
+
+		return client.users.get(str);
+	}
+}
+
 module.exports = async (_y, args) => {
   const uid = _y.message.author.id;
   const user = await User.findOne({where: {uid: uid}});

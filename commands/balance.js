a1f934316bf515c2118b74acbb1c57cb552ce566
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-06-13 09:08:51 +0000

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,4 +1,4 @@
-/* Juve's nub idea
+/* Juve's nub idea*/
 const db = require('../models/index.js');
 const User = db.User;
 
@@ -29,8 +29,8 @@ module.exports = async (_y, args) => {
   }
   return _y.reply(`$${user.coins}`);
 
-};*/
-/* Real code*/
+};
+/* Real code
 const db = require('../models/index.js');
 const User = db.User;
 
@@ -42,3 +42,4 @@ module.exports = async (_y, args) => {
   }
   return _y.reply(`$${user.coins}`);
 };
+*/

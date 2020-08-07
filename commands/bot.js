75a7da56f93dc4d9fc6e5c673c7b448a19ccb0ac
yuulye <yuulye@gmail.com>
2020-08-08 02:02:27 +0700

    update contributors #7

--- commands/bot.js
+++ commands/bot.js
@@ -1,18 +1,23 @@
 module.exports = (_y, args) => {
   const Discord = _y.Discord;
+  const users = [
+    'yuulye',
+    'juve938383',
+    'Pradeep#5986',
+    'MahadhirMahi#7683',
+    'John <3 Genesis',
+  ];
   const botEmbed = new Discord.RichEmbed()
     .setColor('#1d9eaf')
     .setTitle('League Bot')
     .setURL('https://github.com/fifa-mobile')
-    .setAuthor('League Bot')
     .setDescription('Info on League Bot')
     .setThumbnail(
       'https://fifa-mobile.github.io/images/'
       + 'discordbot-bot-discord-'
       + '11563261320iwm1tpnosh.png'
     )
-    .addField('Founders:', '- yuulye\n- juve938383')
-    .addField('MasterOfNubs:', '- Pradeep#5986\n- MahadhirMahi#7683')
+    .addField('Contributors:', users.join('\n'))
     .addField('Server(s):', 'AHQ Leagues', true)
     .addField('Date created:', '2nd November 2019', true)
     .addField('Use:', 'General purpose', true)

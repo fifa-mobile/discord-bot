48980055918795463806b19128dabfcdc4c8fe02
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-08-12 01:10:06 +0200

    Update canvas.js

--- commands/leader/canvas.js
+++ commands/leader/canvas.js
@@ -6,6 +6,7 @@ module.exports = async (_y, args, data) => {
 
   const titleHeight = 24;
   const listHeight = 18;
+  const listWeight = 'Normal';
   const listPad = 8;
   const listTH = listHeight + listPad;
   const height = data.length * listTH + 32;
@@ -59,10 +60,9 @@ module.exports = async (_y, args, data) => {
 
     const user = data[i];
     if( i < 3){
-    ctx.font = `Lighter ${listHeight}px Sans-Serif`;
-    }else{
-    ctx.font = `Normal ${listHeight}px Sans-Serif`;
+    listWeight = 'Lighter';
     }
+    ctx.font = `${listWeight} ${listHeight}px Sans-Serif`;
 
     ctx.textAlign = "left";
     let uname = `${i + 1}. ${user.uname}`.substring(

067bb58e7a8bbcb6d1b5868b07d5d7ebeefe262e
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-08-12 09:02:45 +0200

    Create leader.js

---
+++ commands/files/leader.js
@@ -0,0 +1,8 @@
+**```ℹ | {{prefix}}leader```**
+**Use**: Used to show $pack inventory value LeaderBoard.
+**Command**: `{{prefix}}leader`
+
+**Example**:
+`{{prefix}}leader` -Shows $pack inventory value LeaderBoard.
+
+```> Don't include brackets when using the commands.```

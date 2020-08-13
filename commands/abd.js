743fbc75f68920ac99fb0e6cafd270e394f32810
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-08-13 20:45:52 +0200

    Update abd.js

--- commands/abd.js
+++ commands/abd.js
@@ -20,19 +20,23 @@ module.exports = async (_y, args) => {
     .setTitle('SBC')
     .setDescription('Use $sbc complete [id] to complete a SBC.')
     .setThumbnail('https://raw.githubusercontent.com/fifa-mobile/discord-bot/master/images/sbc.jpg')
-    .addField('Bronze Upgrade:', '4 Bronze players<->1 Silver player')
-    .addField('Silver Upgrade:', '3 Bronze players+1 Silver player<->1 Gold player')
-    .addField('Gold Upgrade:', '2 Bronze players+1 Gold player<->1 Elite 80-85 player')
-    .addField('Weak Elite Upgrade:', '1 Gold player+1 80-85 Elite player<->1 85+ Elite player')
-    .addField('Elite Upgrade:', '1 Gold player+1 85+ Elite player<->1 90-95 Master player')
-    .addField('Weak Master Upgrade:', '1 85+ Elite player+1 90-95 Master player<->1 95+ Master player')
-    .addField('Master Upgrade:', '4 95+ Master player<->1 Legendary player')
-    .addField('Icon '+ weeklyIcon +':', '1 Gold player+1 90-95 Master player+1 95+ Master player<->1 '+ weeklyIcon +' Icon player')
-    .addField('Prime Icon '+ weeklyPrime +':', '3 Icon players+2 Legendary players<->1 '+ weeklyPrime +' Prime Icon player')
+    .addField('1.Bronze Upgrade:', '4 Bronze players<->1 Silver player')
+    .addField('2.Silver Upgrade:', '3 Bronze players+1 Silver player<->1 Gold player')
+    .addField('3.Gold Upgrade:', '2 Bronze players+1 Gold player<->1 Elite 80-85 player')
+    .addField('4.Weak Elite Upgrade:', '1 Gold player+1 80-85 Elite player<->1 85+ Elite player')
+    .addField('5.Elite Upgrade:', '1 Gold player+1 85+ Elite player<->1 90-95 Master player')
+    .addField('6.Weak Master Upgrade:', '1 85+ Elite player+1 90-95 Master player<->1 95+ Master player')
+    .addField('7.Master Upgrade:', '4 95+ Master player<->1 Legendary player')
+    .addField('8.Icon '+ weeklyIcon +':', '1 Gold player+1 90-95 Master player+1 95+ Master player<->1 '+ weeklyIcon +' Icon player')
+    .addField('9.Prime Icon '+ weeklyPrime +':', '3 Icon players+2 Legendary players<->1 '+ weeklyPrime +' Prime Icon player')
     .setFooter('The players used in the challenge will be removed from your inventory, so think well before completing.');
   _y.reply(botEmbed);
   }
-  
+  if( cmd=== complete){
+  if(!args[1]){
+  _y.reply('Please add challenge ID.Check `$sbc` or `$sbc list` for IDs.');
+  }
+  }
   
   
   }

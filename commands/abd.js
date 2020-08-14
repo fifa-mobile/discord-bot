ab0be1baa009d2379a28bc3c52cb516f669eb4c3
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-08-14 12:35:31 +0200

    Added $sbc complete
    So tired!Let's try it out

--- commands/abd.js
+++ commands/abd.js
@@ -14,7 +14,7 @@ module.exports = async (_y, args) => {
   const weeklyPrime = 'Zidane';
   const weeklyPrimeID = 21500198;
   
-  if( !cmd || cmd === list){
+  if(!cmd || cmd === list){
   const botEmbed = new D.RichEmbed()
     .setColor('#74b4c3')
     .setTitle('SBC')
@@ -32,11 +32,165 @@ module.exports = async (_y, args) => {
     .setFooter('The players used in the challenge will be removed from your inventory, so think well before completing.');
   _y.reply(botEmbed);
   }
-  if( cmd=== complete){
-  if(!args[1]){
-  _y.reply('Please add challenge ID.Check `$sbc` or `$sbc list` for IDs.');
+  if( cmd === complete){ 
+  const amountR = 1;
+  if(!args[1] || args[1] > 9 || args[1] < 1){
+  _y.reply('Invalid ID.Check `$sbc` or `$sbc list` for IDs.');
   }
+ if(args[1] === 1){
+ const id1 = 1;
+ const amount1 = 4;
+ const id2 = 0;
+ const amount2 = 0;
+ const id3 = 0;
+ const amount3 = 0;
+ const idR = 2;
+ const img1 = `./images/pack/bronze.png`;
+ const img2 = img1;
+ const img3 = img1;
+ const img4 = img1;
+ const imgR = `./images/pack/silver.png`;
+}else  if(args[1] === 2){
+ const id1 = 1;
+ const amount1 = 3;
+ const id2 = 2;
+ const amount2 = 1;
+ const id3 = 0;
+ const amount3 = 0;
+ const idR = 3;
+ const img1 = `./images/pack/bronze.png`;
+ const img2 = img1;
+ const img3 = img1;
+ const img4 = `./images/pack/silver.png`;
+ const imgR = `./images/pack/gold.png`;
+}else  if(args[1] === 3){
+ const id1 = 1;
+ const amount1 = 2;
+ const id2 = 3;
+ const amount2 = 1;
+ const id3 = 0;
+ const amount3 = 0;
+ const idR = 4;
+ const img1 = `./images/pack/bronze.png`;
+ const img2 = img1;
+ const img3 = `./images/pack/gold.png`;
+ const imgR = `./images/pack/elite.png`;
+}else  if(args[1] === 4){
+ const id1 = 3;
+ const amount1 = 1;
+ const id2 = 4;
+ const amount2 = 1;
+ const id3 = 0;
+ const amount3 = 0;
+ const idR = 5;
+ const img1 = `./images/pack/gold.png`;
+ const img2 = `./images/pack/elite.png`;
+ const imgR = `./images/pack/elite85.png`;
+}else  if(args[1] === 5){
+ const id1 = 3;
+ const amount1 = 1;
+ const id2 = 5;
+ const amount2 = 1;
+ const id3 = 0;
+ const amount3 = 0;
+ const idR = 6;
+ const img1 = `./images/pack/gold.png`;
+ const img2 = `./images/pack/elite85.png`;
+ const imgR = `./images/pack/master.png`;
+}else if(args[1] === 6){
+ const id1 = 5;
+ const amount1 = 1;
+ const id2 = 6;
+ const amount2 = 1;
+ const id3 = 0;
+ const amount3 = 0;
+ const idR = 7;
+ const img1 = `./images/pack/elite85.png`;
+ const img2 = `./images/pack/master.png`;
+ const imgR = `./images/pack/master95.png`;
+}else  if(args[1] === 7){
+ const id1 = 7;
+ const amount1 = 4;
+ const id2 = 0;
+ const amount2 = 0;
+ const id3 = 0;
+ const amount3 = 0;
+ const idR = 8;
+ const img1 = `./images/pack/master95.png`;
+ const img2 = img1;
+ const img3 = img1;
+ const img4 = img1;
+ const imgR = `./images/pack/legend.png`;
+}else  if(args[1] === 8){
+ const id1 = 3;
+ const amount1 = 1;
+ const id2 = 6;
+ const amount2 = 1;
+ const id3 = 7;
+ const amount3 = 1;
+ const idR = 9;
+ const img1 = `./images/pack/gold.png`;
+ const img2 = `./images/pack/master.png`
+ const img3 = `./images/pack/master95.png`
+ const imgR = `./images/pack/silver.png`;
+ const imgR ='https://fifa-mobile.github.io/images/cards/' + `${weeklyIconID}.png`
+}else  if(args[1] === 9){
+ const id1 = 9;
+ const amount1 = 3;
+ const id2 = 8;
+ const amount2 = 2;
+ const id3 = 0;
+ const amount3 = 0;
+ const idR = 10;
+ const img1 = `./images/pack/icon.png`;
+ const img2 = img1;
+ const img3 = img1;
+ const img4 = `./images/pack/legend.png`;
+ const img5 = img4;
+ const imgR ='https://fifa-mobile.github.io/images/cards/' + `${weeklyPrimeID}.png`
+}
+  
+ const {
+      createCanvas, loadImage
+    } = require('canvas');
+
+ const w = 1280;
+ const h = 500;
+ const canvas = createCanvas(w, h);
+ const ctx = canvas.getContext('2d');
+    
+ ctx.drawImage(`./images/switchArrows.png`, 640, 250, 320, 320);
+ ctx.drawImage(imgR, 1040, 250, 400, 400);
+ const insertSize = 128;
+ const totalAmount = amount1 + amount2 + amount3;
+ if(totalAmount === 2){
+ ctx.drawImage(img1, 176, 250, insertSize, insertSize);
+ ctx.drawImage(img2, 304, 250, insertSize, insertSize);
+ }else if(totalAmount === 3){
+ ctx.drawImage(img1, 176, 186, insertSize, insertSize);
+ ctx.drawImage(img2, 304, 186, insertSize, insertSize);
+ ctx.drawImage(img3, 240, 314, insertSize, insertSize);
+ }else if(totalAmount === 4){
+ ctx.drawImage(img1, 176, 186, insertSize, insertSize);
+ ctx.drawImage(img2, 304, 186, insertSize, insertSize);
+ ctx.drawImage(img3, 176, 314, insertSize, insertSize);
+ ctx.drawImage(img4, 304, 314, insertSize, insertSize);
+ }else if(totalAmount === 5){
+ ctx.drawImage(img1, 112, 186, insertSize, insertSize);
+ ctx.drawImage(img2, 240, 186, insertSize, insertSize);
+ ctx.drawImage(img3, 368, 186, insertSize, insertSize);
+ ctx.drawImage(img4, 176, 314, insertSize, insertSize);
+ ctx.drawImage(img5, 304, 314, insertSize, insertSize);
+ }
+ 
+ const attachment = new D.Attachment(canvas.toBuffer(), 'img.png');
+
+ await user.addPack(id1, amount1);
+ await user.addPack(id2, amount2);
+ await user.addPack(id3, amount3);
+ await user.addPack(idR, -amountR); 
+ _y.message.channel.send(`SBC completed!`, {files: [attachment]});
   }
   
   
-  }
+  };

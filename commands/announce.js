e99600b4445ace5d1719dbae7cf47d3d239982a7
yuulye <yuulye@gmail.com>
2020-09-01 19:50:55 +0700

    upgrade #20

--- commands/announce.js
+++ commands/announce.js
@@ -1,6 +1,6 @@
 const y = require('../core/base');
 
-module.exports = (_y, args) => {
+module.exports = async (_y, args) => {
   const m = _y.message;
   if (m.member.roles.find(r => r.name === 'Staff')) {
     if (!args[0]) {
@@ -9,11 +9,42 @@ module.exports = (_y, args) => {
       );
       return;
     }
-    let text = [];
-    for (let i = 0; i < args.length; i++) {
-      text.push(args[i]);
+
+    const axios = require('axios');
+
+    const toAttach = [];
+    const attachments = Array.from(m.attachments);
+    for (let i = 0; i < attachments.length; i++) {
+      try {
+        const a = attachments[i][1];
+
+        const response = await axios.get(
+          a.url,  { responseType: 'arraybuffer' }
+        );
+        const ext = a.url.split('.').pop();
+        const buffer = Buffer.from(
+          response.data, ext
+        );
+
+        toAttach.push({
+          attachment: buffer,
+          name: `file${i}.${ext}`,
+        });
+      } catch (e) {
+        console.log('error getting url', e.message);
+      }
     }
-    m.channel.send(text.join(' '));
+
+    //console.log(toAttach);
+
+    m.channel.send(
+      _y.message.content.replace(
+        /.announce\s+/, ''
+      )
+      , {
+        files: toAttach
+      }
+    );
     m.delete();
   } else {
     m.channel.send(

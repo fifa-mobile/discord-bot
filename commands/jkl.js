2fb529b6cba9cbd23a42efbe25cc2bcf34481980
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-09-03 16:15:39 +0200

    Update jkl.js

--- commands/jkl.js
+++ commands/jkl.js
@@ -3,7 +3,11 @@ module.exports = async (_y, args) => {
   const y = require('../core/base');
   const D = y.Discord;
   
- _y.message.channel.send(`:crabDance: SBC completed!`);
+ const botEmbed = new D.RichEmbed()
+    .setColor('#74b4c3')
+    .setTitle('SBC :crabDance:')
+    .setDescription('Use $sbc complete [id] to complete a SBC. :crabDance:')
+     _y.reply(botEmbed);
   }
   
   

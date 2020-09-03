6a2ca59b1a4ab7447229dbedd078815d884e86bf
yuulye <yuulye@gmail.com>
2020-09-03 19:01:38 +0700

    update abd

--- commands/abd.js
+++ commands/abd.js
@@ -35,199 +35,212 @@ module.exports = async (_y, args) => {
   }
  
   if( cmd === 'complete'){ 
-  const amountR = 1;
-  console.log("complete");
- var img1;
- var img2;
- var img3;
- var img4;
- var img5;
- var imgR;
- var id1;
- var amount1;
- var id2;
- var amount2;
- var id3;
- var amount3;
- var idR;
-  if(!args[1] || args[1] > 9 || args[1] < 1){
-  _y.reply('Invalid ID.Check `$sbc` or `$sbc list` for IDs.');
-  console.log("invalid");
-  }
-  if(args[1] === 1){
- id1 = 1;
- amount1 = 4;
- id2 = 0;
- amount2 = 0;
- id3 = 0;
- amount3 = 0;
- idR = 2;
- img1 = `./images/pack/bronze.png`;
- img2 = img1;
- img3 = img1;
- img4 = img1;
- imgR = `./images/pack/silver.png`;
- console.log("choice1");
-}else  if(args[1] === 2){
- id1 = 1;
- amount1 = 3;
- id2 = 2;
- amount2 = 1;
- id3 = 0;
- amount3 = 0;
- idR = 3;
- img1 = `./images/pack/bronze.png`;
- img2 = img1;
- img3 = img1;
- img4 = `./images/pack/silver.png`;
- imgR = `./images/pack/gold.png`;
- console.log("choice2");
-}else  if(args[1] === 3){
- id1 = 1;
- amount1 = 2;
- id2 = 3;
- amount2 = 1;
- id3 = 0;
- amount3 = 0;
- idR = 4;
- img1 = `./images/pack/bronze.png`;
- img2 = img1;
- img3 = `./images/pack/gold.png`;
- imgR = `./images/pack/elite.png`;
- console.log("choice3");
-}else  if(args[1] === 4){
- id1 = 3;
- amount1 = 1;
- id2 = 4;
- amount2 = 1;
- id3 = 0;
- amount3 = 0;
- idR = 5;
- img1 = `./images/pack/gold.png`;
- img2 = `./images/pack/elite.png`;
- imgR = `./images/pack/elite85.png`;
- console.log("choice4");
-}else  if(args[1] === 5){
- id1 = 3;
- amount1 = 1;
- id2 = 5;
- amount2 = 1;
- id3 = 0;
- amount3 = 0;
- idR = 6;
- img1 = `./images/pack/gold.png`;
- img2 = `./images/pack/elite85.png`;
- imgR = `./images/pack/master.png`;
- console.log("choice5");
-}else if(args[1] === 6){
- id1 = 5;
- amount1 = 1;
- id2 = 6;
- amount2 = 1;
- id3 = 0;
- amount3 = 0;
- idR = 7;
- img1 = `./images/pack/elite85.png`;
- img2 = `./images/pack/master.png`;
- imgR = `./images/pack/master95.png`;
- console.log("choice6");
-}else  if(args[1] === 7){
- id1 = 7;
- amount1 = 4;
- id2 = 0;
- amount2 = 0;
- id3 = 0;
- amount3 = 0;
- idR = 8;
- img1 = `./images/pack/master95.png`;
- img2 = img1;
- img3 = img1;
- img4 = img1;
- imgR = `./images/pack/legend.png`;
- console.log("choice7");
-}else  if(args[1] === 8){
- id1 = 3;
- amount1 = 1;
- id2 = 6;
- amount2 = 1;
- id3 = 7;
- amount3 = 1;
- idR = 9;
- img1 = `./images/pack/gold.png`;
- img2 = `./images/pack/master.png`
- img3 = `./images/pack/master95.png`
- imgR ='https://fifa-mobile.github.io/images/cards/' + `${weeklyIconID}.png`
- console.log("choice8");
-}else  if(args[1] === 9){
- id1 = 9;
- amount1 = 3;
- id2 = 8;
- amount2 = 2;
- id3 = 0;
- amount3 = 0;
- idR = 10;
- img1 = `./images/pack/icon.png`;
- img2 = img1;
- img3 = img1;
- img4 = `./images/pack/legend.png`;
- img5 = img4;
- imgR ='https://fifa-mobile.github.io/images/cards/' + `${weeklyPrimeID}.png`
- console.log("choice9");
-}
-  
- const {
-      createCanvas, loadImage
-    } = require('canvas');
+    const amountR = 1;
+    console.log("complete");
+    var img1;
+    var img2;
+    var img3;
+    var img4;
+    var img5;
+    var imgR;
+    var id1;
+    var amount1;
+    var id2;
+    var amount2;
+    var id3;
+    var amount3;
+    var idR;
 
- const w = 1280;
- const h = 500;
- const canvas = createCanvas(w, h);
- const ctx = canvas.getContext('2d');
-  
- const path = `./images/switchArrows.png`;
- const switchArrows = await loadImage(path);
- ctx.drawImage(switchArrows, 480, 90, 320, 320);
- const canImgR = await loadImage(imgR);
- ctx.drawImage(canImgR, 840, 50, 400, 400);
- const canImg1 = await loadImage(img1);
- const canImg2 = await loadImage(img2);
- const canImg3 = await loadImage(img3);
- const canImg4 = await loadImage(img4);
- const canImg5 = await loadImage(img5);
- const insertSize = 128;
- const totalAmount = amount1 + amount2 + amount3;
- if(totalAmount === 2){
- ctx.drawImage(canImg1, 112, 186, insertSize, insertSize);
- ctx.drawImage(canImg2, 240, 186, insertSize, insertSize);
- console.log("total2");
- }else if(totalAmount === 3){
- ctx.drawImage(canImg1, 112, 122, insertSize, insertSize);
- ctx.drawImage(canImg2, 240, 122, insertSize, insertSize);
- ctx.drawImage(canImg3, 176, 250, insertSize, insertSize);
- console.log("total3")
- }else if(totalAmount === 4){
- ctx.drawImage(canImg1, 112, 122, insertSize, insertSize);
- ctx.drawImage(canImg2, 240, 122, insertSize, insertSize);
- ctx.drawImage(canImg3, 112, 250, insertSize, insertSize);
- ctx.drawImage(canImg4, 240, 250, insertSize, insertSize);
- console.log("total4")
- }else if(totalAmount === 5){
- ctx.drawImage(canImg1, 48, 122, insertSize, insertSize);
- ctx.drawImage(canImg2, 176, 122, insertSize, insertSize);
- ctx.drawImage(canImg3, 304, 122, insertSize, insertSize);
- ctx.drawImage(canImg4, 112, 250, insertSize, insertSize);
- ctx.drawImage(canImg5, 240, 250, insertSize, insertSize);
- console.log("total5")
- }
- 
- const attachment = new D.Attachment(canvas.toBuffer(), 'img.png');
+    if(!args[1] || args[1] > 9 || args[1] < 1){
+      _y.reply('Invalid ID.Check `$sbc` or `$sbc list` for IDs.');
+      console.log("invalid");
+    }
 
- await user.addPack(id1, amount1);
- await user.addPack(id2, amount2);
- await user.addPack(id3, amount3);
- await user.addPack(idR, -amountR); 
- _y.message.channel.send(`SBC completed!`, {files: [attachment]});
+    if(args[1] === 1 || args[1] === '1'){
+      id1 = 1;
+      amount1 = 4;
+      id2 = 0;
+      amount2 = 0;
+      id3 = 0;
+      amount3 = 0;
+      idR = 2;
+      img1 = `./images/pack/bronze.png`;
+      img2 = img1;
+      img3 = img1;
+      img4 = img1;
+      img5 = img1;
+      imgR = `./images/pack/silver.png`;
+      console.log("choice1");
+    }else  if(args[1] === 2){
+      id1 = 1;
+      amount1 = 3;
+      id2 = 2;
+      amount2 = 1;
+      id3 = 0;
+      amount3 = 0;
+      idR = 3;
+      img1 = `./images/pack/bronze.png`;
+      img2 = img1;
+      img3 = img1;
+      img4 = `./images/pack/silver.png`;
+      imgR = `./images/pack/gold.png`;
+      console.log("choice2");
+    }else  if(args[1] === 3){
+      id1 = 1;
+      amount1 = 2;
+      id2 = 3;
+      amount2 = 1;
+      id3 = 0;
+      amount3 = 0;
+      idR = 4;
+      img1 = `./images/pack/bronze.png`;
+      img2 = img1;
+      img3 = `./images/pack/gold.png`;
+      imgR = `./images/pack/elite.png`;
+      console.log("choice3");
+    }else  if(args[1] === 4){
+      id1 = 3;
+      amount1 = 1;
+      id2 = 4;
+      amount2 = 1;
+      id3 = 0;
+      amount3 = 0;
+      idR = 5;
+      img1 = `./images/pack/gold.png`;
+      img2 = `./images/pack/elite.png`;
+      imgR = `./images/pack/elite85.png`;
+      console.log("choice4");
+    }else  if(args[1] === 5){
+      id1 = 3;
+      amount1 = 1;
+      id2 = 5;
+      amount2 = 1;
+      id3 = 0;
+      amount3 = 0;
+      idR = 6;
+      img1 = `./images/pack/gold.png`;
+      img2 = `./images/pack/elite85.png`;
+      imgR = `./images/pack/master.png`;
+      console.log("choice5");
+    }else if(args[1] === 6){
+      id1 = 5;
+      amount1 = 1;
+      id2 = 6;
+      amount2 = 1;
+      id3 = 0;
+      amount3 = 0;
+      idR = 7;
+      img1 = `./images/pack/elite85.png`;
+      img2 = `./images/pack/master.png`;
+      imgR = `./images/pack/master95.png`;
+      console.log("choice6");
+    }else  if(args[1] === 7){
+      id1 = 7;
+      amount1 = 4;
+      id2 = 0;
+      amount2 = 0;
+      id3 = 0;
+      amount3 = 0;
+      idR = 8;
+      img1 = `./images/pack/master95.png`;
+      img2 = img1;
+      img3 = img1;
+      img4 = img1;
+      imgR = `./images/pack/legend.png`;
+      console.log("choice7");
+    }else  if(args[1] === 8){
+      id1 = 3;
+      amount1 = 1;
+      id2 = 6;
+      amount2 = 1;
+      id3 = 7;
+      amount3 = 1;
+      idR = 9;
+      img1 = `./images/pack/gold.png`;
+      img2 = `./images/pack/master.png`
+      img3 = `./images/pack/master95.png`
+      imgR ='https://fifa-mobile.github.io/images/cards/' + `${weeklyIconID}.png`
+      console.log("choice8");
+    }else  if(args[1] === 9){
+      id1 = 9;
+      amount1 = 3;
+      id2 = 8;
+      amount2 = 2;
+      id3 = 0;
+      amount3 = 0;
+      idR = 10;
+      img1 = `./images/pack/icon.png`;
+      img2 = img1;
+      img3 = img1;
+      img4 = `./images/pack/legend.png`;
+      img5 = img4;
+      imgR ='https://fifa-mobile.github.io/images/cards/' + `${weeklyPrimeID}.png`
+      console.log("choice9");
+    }
+
+    console.log('aaaaaaaaaaaaaaaaaaaaaa');
+  
+    const {
+        createCanvas, loadImage
+      } = require('canvas');
+
+    const w = 1280;
+    const h = 500;
+    const canvas = createCanvas(w, h);
+    const ctx = canvas.getContext('2d');
+    console.log('bbbbbbbbbbbbbbb3');
+
+    console.log(imgR, img1, img2, img3, img4, img5);
+    const path = `./images/switchArrows.png`;
+    const switchArrows = await loadImage(path);
+    ctx.drawImage(switchArrows, 480, 90, 320, 320);
+    const canImgR = await loadImage(imgR);
+    ctx.drawImage(canImgR, 840, 50, 400, 400);
+    const canImg1 = await loadImage(img1);
+    const canImg2 = await loadImage(img2);
+    const canImg3 = await loadImage(img3);
+    const canImg4 = await loadImage(img4);
+    const canImg5 = await loadImage(img5);
+    const insertSize = 128;
+    const totalAmount = amount1 + amount2 + amount3;
+    console.log('bbbbbbbbbbbbbbb2');
+
+    if(totalAmount === 2){
+      ctx.drawImage(canImg1, 112, 186, insertSize, insertSize);
+      ctx.drawImage(canImg2, 240, 186, insertSize, insertSize);
+      console.log("total2");
+    }else if(totalAmount === 3){
+      ctx.drawImage(canImg1, 112, 122, insertSize, insertSize);
+      ctx.drawImage(canImg2, 240, 122, insertSize, insertSize);
+      ctx.drawImage(canImg3, 176, 250, insertSize, insertSize);
+      console.log("total3")
+    }else if(totalAmount === 4){
+      ctx.drawImage(canImg1, 112, 122, insertSize, insertSize);
+      ctx.drawImage(canImg2, 240, 122, insertSize, insertSize);
+      ctx.drawImage(canImg3, 112, 250, insertSize, insertSize);
+      ctx.drawImage(canImg4, 240, 250, insertSize, insertSize);
+      console.log("total4")
+    }else if(totalAmount === 5){
+      ctx.drawImage(canImg1, 48, 122, insertSize, insertSize);
+      ctx.drawImage(canImg2, 176, 122, insertSize, insertSize);
+      ctx.drawImage(canImg3, 304, 122, insertSize, insertSize);
+      ctx.drawImage(canImg4, 112, 250, insertSize, insertSize);
+      ctx.drawImage(canImg5, 240, 250, insertSize, insertSize);
+      console.log("total5")
+    }
+    console.log('bbbbbbbbbbbbbbb1');
+
+    const attachment = new D.Attachment(
+      canvas.toBuffer(), 'img.png'
+    );
+
+    await user.addPack(id1, amount1);
+    await user.addPack(id2, amount2);
+    await user.addPack(id3, amount3);
+    await user.addPack(idR, -amountR); 
+
+    console.log('bbbbbbbbbbbbbbb');
+    _y.message.channel.send(`SBC completed!`, {files: [attachment]});
   }
   
-  
-  };
+};

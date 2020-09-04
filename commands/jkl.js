a19bf242c32828c82d5f48ea239266e2955a9020
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-09-04 15:04:15 +0200

    Update jkl.js

--- commands/jkl.js
+++ commands/jkl.js
@@ -6,7 +6,7 @@ module.exports = async (_y, args) => {
  const botEmbed = new D.RichEmbed()
     .setColor('#74b4c3')
     .setTitle('SBC <a:peepoJam:501310424449548293>')
-    .setDescription('Use $sbc complete [id] to complete a SBC. <a:crabDance:751080988250996736>')
+    .setDescription('Use $sbc complete [id] to complete a SBC. <a:peepoJam:501310424449548293>')
      _y.reply(botEmbed);
      _y.reply(`<a:crabDance:751080988250996736>`);
   }

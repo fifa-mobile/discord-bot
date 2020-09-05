ead2770ae67c757b9b63fcdc37d754b9bacd7cf1
yuulye <yuulye@gmail.com>
2020-09-05 07:21:05 +0700

    heroes pagination

--- commands/heroes.js
+++ commands/heroes.js
@@ -1,8 +1,30 @@
 
 module.exports = async (_y, args) => {
+  let page = Number(args[0]);
+  const numberPerPage = 10;
   const heroesData = require('./hero/data');
   const heroes = [];
-  for (let i = heroesData.length - 1; i >= 0 ; i--) {
+
+  if (!args[0]) {
+    page = 0;
+  }
+
+  if (isNaN(page)) {
+    return _y.reply(`${page} is not a number!`);
+  }
+
+  const maxPage = Math.floor(heroesData.length / numberPerPage);
+  if (page > maxPage) {
+    return _y.reply(`The pagination max is ${maxPage}`);
+  }
+
+  const offset = (heroesData.length - 1) - (page * numberPerPage);
+  let limit = (offset - numberPerPage) + 1;
+  limit = limit < 0 ? 0 : limit;
+  for (
+    let i = offset;
+    i >= limit ; i--
+  ) {
     const hero = heroesData[i];
     const text = `${hero.name} (${hero.fullName})`;
     let collapse = '...';
@@ -17,5 +39,12 @@ module.exports = async (_y, args) => {
       (i + 1).toString().padStart(3, ' ')
     );
   }
-  _y.replyCode(heroes.join('\n'));
+
+  _y.replyCode(
+    heroes.join('\n')
+    +
+    `\n\n----------------`
+    +
+    `${page}/${maxPage} page`
+  );
 };

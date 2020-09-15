ffdd25a6ead22c6b61c43a351e3a5a50d0c9f6a5
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-09-15 19:03:40 +0200

    Update jkl.js

--- commands/jkl.js
+++ commands/jkl.js
@@ -38,57 +38,57 @@ console.log('var');
     }
    
    if(hasVinicius){
-      p2id = `21238794`;
+      p2id = '21238794';
     }else{
-      p2id = `21238794B`;
+      p2id = '21238794B';
     }
    
     if(hasHazard){
-      p3id = `21183277`;
+      p3id = '21183277';
     }else{
-      p3id = `21183277B`;
+      p3id = '21183277B';
     }
   
     if(hasCasemiro){
-      p4id = `21200145`;
+      p4id = '21200145';
     }else{
-      p4id = `21200145B`;
+      p4id = '21200145B';
     }
   
     if(hasKroos){
-      p5id = `21182521`;
+      p5id = '21182521';
     }else{
-      p5id = `21182521B`;
+      p5id = '21182521B';
     }
   if(hasModric){
-      p6id = `21177003`;
+      p6id = '21177003';
     }else{
-      p6id = `21177003B`;
+      p6id = '21177003B';
     }
   if(hasMarcelo){
-      p7id = `21176676`;
+      p7id = '21176676';
     }else{
-      p7id = `21176676B`;
+      p7id = '21176676B';
     }
   if(hasRamos){
-      p8id = `21155862`;
+      p8id = '21155862';
     }else{
-      p8id = `21155862B`;
+      p8id = '21155862B';
     }
   if(hasVarane){
-      p9id = `21201535`;
+      p9id = '21201535';
     }else{
-      p9id = `21201535B`;
+      p9id = '21201535B';
     }
   if(hasCarvajal){
-      p10id = `21204963`;
+      p10id = '21204963';
     }else{
-      p10id = `21204963B`;
+      p10id = '21204963B';
     }
   if(hasCourtois){
-      p11id = `21192119`;
+      p11id = '21192119';
     }else{
-      p11id = `21192119B`;
+      p11id = '21192119B';
     }
 console.log('id');
       const p1 ='https://raw.githubusercontent.com/fifa-mobile/discord-bot/master/images/TestImg/' + p1id +'.png'
@@ -112,7 +112,7 @@ console.log('id');
     const pitch = 'https://fifa-mobile.github.io/images/TestImg/pitch.png'
     
     const canP1 = await loadImage(p1);
-  console.log('img1 loaded');
+  
     const canP2 = await loadImage(p2);
     const canP3 = await loadImage(p3);
     const canP4 = await loadImage(p4);
@@ -125,7 +125,7 @@ console.log('id');
     const canP11 = await loadImage(p11);
     const canPitch = await loadImage(pitch);
     
-   
+   console.log('images loaded');
     
     const s = 192;
     ctx.drawImage(canPitch, 0, 0, w, h);

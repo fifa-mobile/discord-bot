d672474b63281db6b7f00be7931543435a4b5644
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-09-18 19:07:06 +0200

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -17,7 +17,7 @@ module.exports = async (_y, args) => {
    /*const img = await loadImage(url);*/
    const attachment = new D.MessageAttachment(url, 'AHQH2H.gif');
 
-   m.channel.send("Hello @everyone ! \nIt is time for another **AHQ Tournament**!!! <a:1blobDance:739413657028526091> \nThis time it is a special one and we will reveal more regarding this in the upcoming days. \n\nSo let's get to action, shall we? \nTo enter the Tournament all you need to do is click the reaction below. Keep in mind that this Tournament will have some restrictions regarding team building but don't worry, they aren't too hard. See you on the virtual pitch! <a:football:751811132834054255> :vamos:", {files: [attachment]})
+   m.channel.send("Hello @everyone ! \nIt is time for another **AHQ Tournament**!!! <a:1blobDance:739413657028526091> \nThis time it is a special one and we will reveal more regarding this in the upcoming days. \n\nSo let's get to action, shall we? \nTo enter the Tournament all you need to do is click the reaction below. Keep in mind that this Tournament will have some restrictions regarding team building but don't worry, they aren't too hard. \nSee you on the virtual pitch! <a:football:751811132834054255> <:vamos:683393120200556556>", {files: [attachment]})
    m.delete();
    }else{
     const axios = require('axios');

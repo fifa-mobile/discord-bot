979a72da13120f6c7771d752cf78975b0bcb4f4b
juve938383 <58861443+juve938383@users.noreply.github.com>
2020-09-18 18:11:24 +0200

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -12,7 +12,13 @@ module.exports = async (_y, args) => {
       return;
     }
    if(args[0]==='ejiwoqhdieks'){
+   const D = y.Discord;
+   const url = 'https://raw.githubusercontent.com/fifa-mobile/discord-bot/master/images/AHQH2H.gif';
+   const img = await loadImage(url);
+   const attachment = new D.MessageAttachment(img, 'x.png');
+
    m.channel.send("Hello @everyone ! \nIt is time for another **AHQ Tournament**!!! <a:1blobDance:739413657028526091> \nThis time it is a special one and we will reveal more regarding this in the upcomming days. \nSo let's get to action, shall we? \nTo enter the Tournament all you need to do is click the reaction below. Keep in mind that this Tournament will have some restrictions regarding team building but don't worry, they aren't too hard. See you on the virtual pitch! <a:football:751811132834054255>")
+   m.delete();
    }else{
     const axios = require('axios');
 

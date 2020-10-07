334d5e244d1c5848e6330282658809af7368899a
yuulye <yuulye@gmail.com>
2020-10-07 15:45:11 +0700

    web cards data fetching

--- routes/cards.js
+++ routes/cards.js
@@ -4,7 +4,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   (async function() {
     const Card = require('../models/mongoose/card');
-    const cards = await Card.find({});
+    const cards = await Card.find({}, null, {
+      sort: {updatedAt: 'desc'}
+    }).exec();
     res.render('cards', {
       title: 'Card List',
       layout: 'layouts/base',

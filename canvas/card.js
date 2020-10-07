e75ff605600cc68c84bbfb12e2346b1a511a2930
yuulye <yuulye@gmail.com>
2020-10-07 21:55:46 +0700

    add fonts

--- canvas/card.js
+++ canvas/card.js
@@ -1,5 +1,20 @@
 module.exports = async (card) => {
-  const { createCanvas, loadImage } = require('canvas');
+  const {
+    createCanvas, loadImage, registerFont
+  } = require('canvas');
+
+  registerFont(
+    './public/fonts/Posterama-Bold.ttf',
+    {
+      family:'PosteramaBold'
+    }
+  );
+  registerFont(
+    './public/fonts/DINPro-CondBold.woff',
+    {
+      family: 'DINProCondBold'
+    }
+  );
 
   const canvas = createCanvas(256, 256);
   const ctx = canvas.getContext('2d');
@@ -10,19 +25,19 @@ module.exports = async (card) => {
   image = await loadImage(card.playerImg);
   ctx.drawImage(image, 0, 0);
 
-  ctx.font = 'condensed 32px dinpro';
+  ctx.font = '55px DINProCondBold';
   ctx.fillStyle = '#eee';
   ctx.strokeStyle = '#111';
   ctx.lineWidth = 4;
 
-  ctx.strokeText(card.rating, 60, 60);
-  ctx.fillText(card.rating, 60, 60);
+  ctx.strokeText(card.rating, 57, 62);
+  ctx.fillText(card.rating, 57, 62);
 
   ctx.textAlign = 'center';
-  ctx.font = 'bold 12px posterama';
+  ctx.font = 'bold 17px PosteramaBold';
 
-  ctx.strokeText(card.position, 78, 80);
-  ctx.fillText(card.position, 78, 80);
+  ctx.strokeText(card.position, 78, 84);
+  ctx.fillText(card.position, 78, 84);
 
   image = await loadImage(card.clubImg);
   ctx.drawImage(image, 58, 90, 38, 38);
@@ -30,10 +45,10 @@ module.exports = async (card) => {
   image = await loadImage(card.nationImg);
   ctx.drawImage(image, 62, 140, 30, 18);
 
-  ctx.font = 'bold 16px posterama';
+  ctx.font = 'bold 21px PosteramaBold';
 
-  ctx.strokeText(card.name, 128, 194);
-  ctx.fillText(card.name, 128, 194);
+  ctx.strokeText(card.name, 128, 197);
+  ctx.fillText(card.name, 128, 197);
 
   const imgBuf = canvas.toBuffer('image/png');
 
